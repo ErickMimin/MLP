7598da28f49cce6a2076ad33430fad388bf99df6
ErickMimin <Eri_mimo@hotmail.com>
2019-05-12 22:35:39 -0500

    Valores dados por el usuario

---
+++ MultiLayerPerceptron.m
@@ -0,0 +1,27 @@
+%Limpiamos datos
+clear;clc;close all;
+%Archivo de entrada 
+inputFile = input('Ingrese el nombre del archivo con los datos de entrada P(.txt): ','s');
+input = load(inputFile + '.txt');
+%Archivo de targets
+targetFile = input('Ingrese el nombre del archivo con los targets(.txt):','s');
+target = load(targetFile + '.txt');
+%Rango de la señal
+range = input('Ingrese el rango de la senal a aproximar [rango-minimo , rango-maximo]: ');
+%Arquitectura del MLP
+layerVector = input('Ingrese el vector de entradas de cada capa [1,S^1,S^2,...,S^n,1]: ');
+functionVector = input('Ingrese el vector de funciones de cada capa [1,2,3,...,2,1,3]: ');
+%Factor de aprendizaje
+learningRate = input('Ingrese el factor de aprendizaje dentro del rango 0 a 1: ');
+%Condiciones de finalizacion
+epochmax = input('Ingrese el numero maximo de epocas (epochmax): ');
+min_error_train = input('Ingrese el valor maximo de error aceptado: ');
+%Configuracion de validacion
+epochval = input('Ingrese el numero de epocas de entrenamiento entre cada epoca de validacion: ');
+numval = input('Ingrese el numero maximo de incrementos consecutivos de error de validacion: ');
+%Configuracion del dataset 
+if(input('Ingrese 1) 80%-10%-10% o 2) 70%-15%-15%') == 1)
+    datasetConfig = [80 10 10];
+else
+    datasetConfig = [70 15 15];
+end
