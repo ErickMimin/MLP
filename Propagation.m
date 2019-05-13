29f3f2e71fcc14c78bba2781e3c8bfcc6661827a
ErickMimin <Eri_mimo@hotmail.com>
2019-05-13 00:33:53 -0500

    Particion del dataset y arreglo de error

---
+++ Propagation.m
@@ -0,0 +1,8 @@
+function [result] = Propagation(vetor1, vector2)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+%outputArg1 = inputArg1;
+%outputArg2 = inputArg2;
+result = 0;
+end
+
