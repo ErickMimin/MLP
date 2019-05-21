4abe24faa88726ce96e69a4293b24bc6cac3b335
Sebas P <sparedesl1200@egresado.ipn.mx>
2019-05-20 21:50:06 -0500

    Cambio en el algoritmo EarlyStopping

--- EarlyStopping.m
+++ EarlyStopping.m
@@ -1,17 +1,17 @@
-function increases = EarlyStopping(functionVector, mlpParam, validation,target)
+function increases = EarlyStopping(functionVector, mlpParam, validation)
     for i=1:size(validation, 1)
         a = Propagation(functionVector, mlpParam(end, :), transpose(validation(i, :)));
         if(i==1)
-            error = abs(target(i)-a);
+            error = abs(validation(i, 2)-a);
         end
         
-        if(abs(target(i)-a)>error)
+        if(abs(validation(i, 2)-a)>error)
             increases = increases + 1;
         else
             increases = 0;
         end
         
-        error = abs(target(i)-a);
+        error = abs(validation(i, 2)-a);
     end
 end
 
