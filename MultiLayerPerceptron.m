940fc2d68b47df15fc5121c346c0333b5bdd4754
Sebas P <sparedesl1200@egresado.ipn.mx>
2019-05-28 13:23:20 -0500

    Guardar y cargar pesos y bias

--- MultiLayerPerceptron.m
+++ MultiLayerPerceptron.m
@@ -1,17 +1,17 @@
 %Limpiamos datos
-clear;clc;close all;
+clear;clc;close all;mkdir('.', 'param')
 %====================== Introduccion de datos ======================%
 %Archivo de entrada 
-inputFile = 'Dataset';%input('Ingrese el nombre del archivo con los datos de entrada P(.txt): ', 's');
+inputFile = '01_Polinomio_Entrada';%input('Ingrese el nombre del archivo con los datos de entrada P(.txt): ', 's');
 p = load(strcat(inputFile, '.txt'));
 %Archivo de targets
-targetFile = 'Target';%input('Ingrese el nombre del archivo con los targets(.txt): ', 's');
+targetFile = '01_Polinomio_Target';%input('Ingrese el nombre del archivo con los targets(.txt): ', 's');
 target = load(strcat(targetFile, '.txt'));
 % %Rango de la señal
 % range = input('Ingrese el rango de la senal a aproximar [rango-minimo , rango-maximo]:');
 %Arquitectura del MLP
-layerVector = [1 2 1];%input('Ingrese el vector de entradas de cada capa [1,S^1,S^2,...,S^n,1]: ');
-functionVector = [2 1];%input('Ingrese el vector de funciones de cada capa [1,2,3,...,2,1,3]: ');
+layerVector = [1 8 1];%input('Ingrese el vector de entradas de cada capa [1,S^1,S^2,...,S^n,1]: ');
+functionVector = [3 1];%input('Ingrese el vector de funciones de cada capa [1,2,3,...,2,1,3]: ');
 %Factor de aprendizaje
 learningRate = 1 * 10^(-2);%input('Ingrese el factor de aprendizaje dentro del rango 0 a 1: ');
 %Condiciones de finalizacion
@@ -20,6 +20,7 @@ min_error_train = input('Ingrese el valor minimo de error aceptado: ');
 %Configuracion de validacion
 epochval = input('Ingrese el numero de epocas de entrenamiento entre cada epoca de validacion: ');
 numval = input('Ingrese el numero maximo de incrementos consecutivos de error de validacion: ');
+aleat = input('¿Desea inicializar los valores de pesos y bias aleatoriamente?(1=Si/0=No)');
 % Una prueba de backpropagation
 % erick = BackPropagation({{[-0.27;-0.41], [0.09 -0.17]},{[-0.48;-0.13], 0.48}}, 0.1, {1;[0.321;0.368];0.446}, 1.261 + 0.446,[2 1]);
 % erick2 = Propagation([2 1], {{[-0.27;-0.41], [0.09 -0.17]},{[-0.48;-0.13], 0.48}}, 1);
@@ -30,17 +31,47 @@ mlpParam = cell(1 ,2);
 mlpParam{1} = cell(size(layerVector, 2) - 1, 1);
 %2 .- Celda de bias
 mlpParam{2} = cell(size(layerVector, 2) - 1, 1);
-%Generacion aleatoria de pesos y bias
-for i = 2 : size(layerVector, 2)
-    mlpParam{1}{i - 1} = -1 + (1+1) * rand(layerVector(i), layerVector(i - 1));
-    mlpParam{2}{i - 1} = -1 + (1+1) * rand(layerVector(i), 1);
+if aleat == 1
+    %Generacion aleatoria de pesos y bias
+    for i = 2 : size(layerVector, 2)
+        mlpParam{1}{i - 1} = -1 + (1+1) * rand(layerVector(i), layerVector(i - 1));
+        mlpParam{2}{i - 1} = -1 + (1+1) * rand(layerVector(i), 1);
+    end
+else
+    for i = 2:size(layerVector, 2)
+        %Cargar pesos
+        j = i - 1;
+        fileID = fopen("./param/PesosFinal" + (i-1) + ".txt", 'r');
+        mlpParam{1, 1}{i-1, 1} = fscanf(fileID, '%f', [layerVector(i) Inf]);
+        fclose(fileID);
+        
+        %Cargar bias
+        fileID = fopen("./param/BiasFinal" + (i-1) + ".txt", 'r');
+        mlpParam{1, 2}{i-1, 1} = fscanf(fileID, '%f', [layerVector(i) Inf]);
+        fclose(fileID);
+    end
 end
+
+for i = 1:size(layerVector, 2)-1
+    %Guardar pesos
+    fileID = fopen("./param/Pesos" + i + ".txt", 'w');
+    fprintf(fileID, '%d ', mlpParam{1, 1}{i, 1});
+    fclose(fileID);
+    %Guardar bias
+    fileID = fopen("./param/Bias" + i + ".txt", 'w');
+    fprintf(fileID, '%d ', mlpParam{1, 2}{i, 1});
+    fclose(fileID);
+end
+
+
 %Ahora vamos a separar los datos, usamos la funcion separarDatos pasando
 %como parametros el arreglo/matriz de inputs y el arreglo/matriz de files
 %nos regresa 3 matrices 2 filan x n columnas (depende del numero de datos
 %en total
 %para acceder a los datos Ejemplo conjValidacion(1,x) dato input y
 %conjvaldacion(2,x) target respectivo
+
+
 [conjAprendizaje,conjValidacion,conjPrueba] = SeparateData(p,target);
 
 a = cell(size(conjAprendizaje, 2), 1);
@@ -64,15 +95,15 @@ for epoch = 1: epochmax
        end
        %increases = EarlyStopping(functionVector, mlpParam(end, :), conjValidacion);
        if(increases == numval)
-           mlpParam(size(mlpParam, 1) + 1, :) = mlpParamBeIn;
-           fprintf("EarlyStopping paro el programa\n");
+           mlpParam(1, :) = mlpParamBeIn;
+           fprintf("EarlyStopping paro el programa\n" + epoch);
            break;
        end
     else
         for i = 1:size(conjAprendizaje, 2)
            a{i} = Propagation(functionVector, mlpParam(end, :), conjAprendizaje(1, i));
            if(~isequal(conjAprendizaje(2, i), a{i}{end}))
-               mlpParam(size(mlpParam, 1) + 1, :) = BackPropagation(mlpParam(end, :), learningRate, a{i}, conjAprendizaje(2, i), functionVector);
+               mlpParam(1, :) = BackPropagation(mlpParam(end, :), learningRate, a{i}, conjAprendizaje(2, i), functionVector);
            end
            % Suma de errores
            errorEpoch(epoch) = errorEpoch(epoch) + abs(conjAprendizaje(2, i) - a{i}{end});
@@ -85,8 +116,34 @@ for epoch = 1: epochmax
             break;
         end
     end
+    if(mod(epoch, 100) == 0)
+        disp("Epoca: " + epoch);
+        for i = 1:size(layerVector, 2)-1
+            %Guardar pesos
+            fileID = fopen("./param/Pesos" + i + ".txt", 'a');
+            fprintf(fileID, '%d ', mlpParam{1, 1}{i, 1});
+            fclose(fileID);
+            %Guardar bias
+            fileID = fopen("./param/Bias" + i + ".txt", 'a');
+            fprintf(fileID, '%d ', mlpParam{1, 2}{i, 1});
+            fclose(fileID);
+        end
+    end
 end
-% Epoca de validacion
+
+%Guardar pesos y bias finales
+for i = 1:size(layerVector, 2)-1
+    %Guardar pesos
+    fileID = fopen("./param/PesosFinal" + i + ".txt", 'w');
+    fprintf(fileID, '%d ', mlpParam{1, 1}{i, 1});
+    fclose(fileID);
+    %Guardar bias
+    fileID = fopen("./param/BiasFinal" + i + ".txt", 'w');
+    fprintf(fileID, '%d ', mlpParam{1, 2}{i, 1});
+    fclose(fileID);
+end
+
+% Epoca de prueba
 aTest = cell(size(conjPrueba, 2), 1);
 errorTest = 0;
 for i = 1:size(conjPrueba, 2)
@@ -127,5 +184,11 @@ title("Target vs Salida");
 xlabel('X');
 ylabel('Y');
 hold off;
+figure
+hold on;
+plot(conjPrueba(1, :), aTestG(1, :));
+plot(conjPrueba(1, :), conjPrueba(2, :));
+hold off;
+
 
 
